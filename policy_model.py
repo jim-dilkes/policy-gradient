8dc71432077604ace875cdd399b2b7c2089403e3
Jim <jsdilkes@googlemail.com>
2023-12-01 13:41:27 +0000

    Refactor in to Agent classes

--- policy_model.py
+++ policy_model.py
@@ -1,6 +1,8 @@
 import gymnasium as gym
 import torch
 from torch import nn
+import torch.nn.functional as F
+from functools import partial
 
 class PyTorchWrapper(gym.Wrapper):
     def __init__(self, env, device):
@@ -18,10 +20,9 @@ class PyTorchWrapper(gym.Wrapper):
 
 
 class MultiLayerModel(nn.Module):
-    def __init__(self, n_env_inputs, n_action_space, n_per_hidden, activation=nn.ReLU, output_activation=nn.Softmax):
+    def __init__(self, n_env_inputs, n_action_space, n_per_hidden, output_activation, activation=nn.ReLU):
         super().__init__()
         n_per_hidden = [n_per_hidden] if type(n_per_hidden) != list else n_per_hidden
-        
         layer_counts = [n_env_inputs] + n_per_hidden + [n_action_space]
         
         layers = []
@@ -35,6 +36,131 @@ class MultiLayerModel(nn.Module):
         
     def forward(self, x):
         logits = self.linear_relu_stack(x)
-        actions = self.output(logits)
-        return actions
-    
+        return self.output(logits)
+
+
+class Agent():
+    def __init__(self, n_env_inputs, n_action_space, n_per_hidden, batch_size=1,
+                policy_optimizer=torch.optim.Adam,
+                policy_lr=1e-3):
+        self.policy_model = MultiLayerModel(n_env_inputs, n_action_space, n_per_hidden, partial(torch.nn.Softmax, dim=-1), activation=nn.ReLU)
+        
+        self.rewards = []
+        self.log_probs = []
+
+        self.policy_optimizer = policy_optimizer(self.policy_model.parameters(), lr=policy_lr)
+
+        self.batch_size = batch_size
+        self.batch_count = 0
+
+    def set_policy_learning_rate(self, lr):
+        for param_group in self.policy_optimizer.param_groups:
+            param_group['lr'] = lr
+
+    def save(self, path):
+        if path[-3:] != ".pt":
+            path += ".pt"
+        torch.save(self.policy_model.state_dict(), path)
+
+    def load_state_dict(self, state_dict):
+        self.policy_model.load_state_dict(state_dict)
+
+    def to(self, device):
+        self.policy_model.to(device)
+        return self
+
+    def reset(self):
+        self.rewards = []
+        self.log_probs = []
+        self.batch_count = 0
+
+        self.policy_optimizer.zero_grad()
+
+    def select_action(self, state):
+        probs = self.policy_model.forward(state)
+        dist = torch.distributions.Categorical(probs)
+        action = dist.sample()
+        self.log_probs.append(dist.log_prob(action))
+        return action.item()
+
+    def store_reward(self, reward):
+        self.rewards.append(reward)
+
+    def calculate_returns(self, gamma=1, normalize=True):
+        R = 0
+        returns = []
+        for r in self.rewards[::-1]:
+            R = r + gamma * R
+            returns.insert(0, R)
+        returns = torch.tensor(returns)
+        if normalize:
+            returns = (returns - returns.mean()) / (returns.std() + 1e-5)
+        return returns
+
+    def update_parameters(self, gamma=1):
+        returns = self.calculate_returns(gamma, normalize=True)
+        loss = -torch.stack(self.log_probs).squeeze() * returns
+        loss = sum(loss)
+        loss.backward()
+        self.policy_optimizer.step()
+        self.policy_optimizer.zero_grad()
+
+        del self.rewards[:]
+        del self.log_probs[:]
+
+
+class BaselineAgent(Agent):
+    '''Agent with a value model to estimate the value of a state.'''
+    def __init__(self, n_env_inputs, n_action_space, n_per_hidden, batch_size=1,
+                 policy_optimizer=torch.optim.Adam, policy_lr=1e-3,
+                 value_optimizer=torch.optim.Adam, value_lr=1e-2):
+        super().__init__(n_env_inputs, n_action_space, n_per_hidden, batch_size, policy_optimizer, policy_lr)
+        self.value_model = MultiLayerModel(n_env_inputs, 1, n_per_hidden, None, activation=nn.ReLU)
+        self.value_optimizer = value_optimizer(self.value_model.parameters(), lr=value_lr)
+
+        self.states = []
+
+    def select_action(self, state):
+        action = super().select_action(state)
+        self.states.append(state)
+        return action
+
+    def to(self, device):
+        self.value_model.to(device)
+        super().to(device)
+        return self
+
+    def reset(self):
+        super().reset()
+        self.states = []
+        self.value_optimizer.zero_grad()
+
+    def set_value_learning_rate(self, lr):
+        for param_group in self.value_optimizer.param_groups:
+            param_group['lr'] = lr
+
+    def update_parameters(self, gamma=1):
+
+        # Calculate advantages
+        returns = self.calculate_returns(gamma, normalize=False)
+        value_estimates = self.value_model(torch.stack(self.states)).squeeze()
+        advantages = (returns - value_estimates).detach()
+
+        # Update value model
+        value_loss = -value_estimates * advantages
+        value_loss = value_loss.sum()
+        value_loss.backward()
+        self.value_optimizer.step()
+        self.value_optimizer.zero_grad()
+
+        # Update policy model
+        policy_loss = -torch.stack(self.log_probs).squeeze() * advantages
+        policy_loss = sum(policy_loss)
+        policy_loss.backward()
+        self.policy_optimizer.step()
+        self.policy_optimizer.zero_grad()
+
+        # Clear memory
+        del self.rewards[:]
+        del self.log_probs[:]
+        del self.states[:]
--- run_saved_model.py
+++ run_saved_model.py
@@ -1,35 +1,49 @@
 import gymnasium as gym
 import torch
-from policy_model import PyTorchWrapper, MultiLayerModel
+from policy_model import PyTorchWrapper, Agent
+from functools import partial
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using device: {device}")
 
 env_name = "LunarLander-v2"
 # env_name = "CartPole-v1"
 
-env = PyTorchWrapper(gym.make(env_name, render_mode="human"))
+env = PyTorchWrapper(gym.make(env_name, render_mode="human"), device)
 n_inputs = env.observation_space.shape[0]
 n_actions = env.action_space.n
 
-model_layers = [128, 64, 64]
+model_layers = [128,64]
+output_activation = partial(torch.nn.Softmax, dim=-1)
+model = Agent(n_inputs, n_actions, model_layers)
 
-model = MultiLayerModel(n_inputs, n_actions, model_layers)
 # Load pytorch model
-state_dict = torch.load(f"{env_name}.pt", map_location=torch.device('cpu'))
+state_dict = torch.load(f"{env_name}.pt", map_location=torch.device(device))
 model.load_state_dict(state_dict)
 
 # Load openai lunar lander env
 # env = PyTorchWrapper(gym.make("CartPole-v1", render_mode="human"))
-n_steps = 1000
+n_steps = 500
 observation, info = env.reset()
 print("Running")
+
+cum_reward = 0
 for i in range(n_steps):
     env.render()
-    action_dist = model(observation)
-    action = torch.argmax(action_dist)
-    observation, reward, terminated, truncated, info = env.step(action.item())
+    action = model.select_action(observation)
+    observation, reward, terminated, truncated, info = env.step(action)
+    cum_reward += reward
 
     if terminated or truncated:
         break
-print(f"Done after {i} steps")
+    
+print(f"Done after {i} steps with cumulative reward {cum_reward}")
 
 env.close()
 
